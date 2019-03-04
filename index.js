d55c77363c0f92f20a57295933149ce90b3b54a1
minhl337 <imnotminh@gmail.com>
2019-03-04 21:20:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-var recipes={};
+// var recipes={};
 
-function updateObjectWithKeyAndValue(x,y,z){
-  return Object.assign({},x,{[y]:z})
-}
+// function updateObjectWithKeyAndValue(x,y,z){
+//   return Object.assign({},x,{[y]:z})
+// }
 
-function destructivelyUpdateObjectWithKeyAndValue(x,y,z){
-  x[y]=z
-  return x
-}
+// function destructivelyUpdateObjectWithKeyAndValue(x,y,z){
+//   x[y]=z
+//   return x
+// }
 
-function deleteFromObjectByKey(x,y){
-  var k={}
-  var k=Object.assign({},x)
-  delete k[y];
-  return k
-}
+// function deleteFromObjectByKey(x,y){
+//   var k={}
+//   var k=Object.assign({},x)
+//   delete k[y];
+//   return k
+// }
 
-function destructivelyDeleteFromObjectByKey(x,y){
-  delete x[y];
-  return x
-}
+// function destructivelyDeleteFromObjectByKey(x,y){
+//   delete x[y];
+//   return x
+// }
